4cb8ee428ee9dbbe8a62b31db47875171d839c0f
Karla Hugo <karla.hugo@granicus.com>
2017-08-16 16:07:46 -0500

    beginning to display book details

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function(){
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' },
-    { title: 'Encyclopedia Britannica Volume 3'}
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 300 },
+    { title: 'The Dark Tower', pages: 85 },
+    { title: 'Eloquent Ruby', pages: 24 },
+    { title: 'Encyclopedia Britannica Volume 3', pages: 899}
   ]
 }
